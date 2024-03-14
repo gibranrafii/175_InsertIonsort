ebda17cafbf53575d6afdc4cfa55ffaa6233c7e4
gibranrafii <gibranrafi21@gmail.com>
2024-03-14 10:55:37 +0700

    membuat prosedure display

--- Algoritma2.cpp
+++ Algoritma2.cpp
@@ -49,9 +49,19 @@ void insertionsort() {						//procedure Insertionsort
 	}
 }
 
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
 
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
+}
 
 int main()
 {
-    
+	
 }
