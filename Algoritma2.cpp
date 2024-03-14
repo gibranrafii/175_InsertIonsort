85d4449517388c64e6b8947f118ca36a0a9da029
gibranrafii <gibranrafi21@gmail.com>
2024-03-14 10:28:31 +0700

    menyelesaikan void input dan memberikan keterangan nya

--- Algoritma2.cpp
+++ Algoritma2.cpp
@@ -5,18 +5,25 @@ int arr[20];
 int n;
 
 void input() {
-    while (true);
-    {
-        cout << "Masukkan Jumlah Data pada Array : ";
-        cin >> n;
-        if (n <= 20) {
-            break;
-        }
-        else {
-            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
-        }
-    }
-    cout << endl;
+	while (true) {													//looping
+		cout << "Masukkan Banyaknya elemen pada array : ";			//output ke layar
+		cin >> n;													//input dari user
+		if (n <= 20)												//jika n berkurang dari atau sama dengan 20
+			break;													//keluar dari loop
+		else {														//jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maks 20 elemen.\n";	//output jika lebih dari 20
+		}
+	}
+	cout << endl;								//membuat jarak per baris program
+	cout << "======================" << endl;	//membuat tampilan susunan data element array
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)					//menggunakan pengulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";	//memasukkan atau menginputkan nilai data n
+		cin >> arr[i];							//menyimpan nilai data n kedalam array arr
+	}
 }
 
 
