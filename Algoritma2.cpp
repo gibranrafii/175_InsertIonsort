a115146230f05728c0e74e626adc372c955f005d
gibranrafii <gibranrafi21@gmail.com>
2024-03-14 10:16:07 +0700

    Add project files.

---
+++ Algoritma2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+    while (true);
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";
+        cin >> n;
+        if (n <= 20) {
+            break;
+        }
+        else {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
+        }
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    
+}
